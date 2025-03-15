9fed2624dc1e357ba0df238a6fda00a834a256ee
Maedeh Mahmoudi <maedeemahmoudi5@gmail.com>
2025-03-15 21:35:47 +0330

    example 1

--- generator.py
+++ generator.py
@@ -1,1 +1,11 @@
 #generator in python
+#example1
+
+def square_number(nums):
+    for i in nums:
+        yield(i*i)
+        
+my_nums = square_number([1,2,3,4,5])
+
+for number in my_nums:
+    print(number)
